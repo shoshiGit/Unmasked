d666d7f19564629e79346317d77dc084f7d2be31
shoshiHub <shoshiwolpe@gmail.com>
2024-03-07 09:12:55 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import ProductList from "./features/product/ProductList";
+import NavBar from "./NavBar";
+import ProductDetails from "./features/product/ProductDetails";
+import OneProduct from "./features/product/OneProduct";
+import SignUp from "./features/user/SingUp";
+import Login from "./features/user/Login";
+import SmallBasket from "./features/order/SmallBasket";
+import Basket from "./features/order/Basket";
+import OrderDetails from "./features/order/OrderDetails"
+//npm i mdb-react-ui-kit
+//npm i @fortawesome/fontawesome-free
+//npm i --save mdb-react-ui-kit 
+//npm install @mui/material @emotion/react @emotion/styled      
+//npm install @mui/icons-material      
+function App() {
+const [currentUser,setCurrentUser]=useState('');
+if(currentUser){
+  console.log(currentUser)
+}
+  return (
+    <>
+      <NavBar />
+ 
+      <Routes>
+
+      <Route path="list" element={<ProductList />}>
+          <Route path=":id" one element={<ProductDetails />} />
+        </Route>
+        <Route path="register" element={<SignUp />} />
+        <Route path="login" element={<Login />} />
+        <Route path="basket" element={<Basket />} />
+        <Route path="SmallBasket" element={<SmallBasket />} />
+        <Route path="orderDetails" element={<OrderDetails/>}/>
+        {/* <Route  path="*" element={<Home/>}/> */}
+      </Routes>
+    </>
+  );
+}
+
+export default App;
+
---
+++ src/Home.js
@@ -0,0 +1,22 @@
+
+// import './home.css';
+// const Home = () => {
+//     return (<>
+//      <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
+//             <div className="carousel-inner">
+//               <div className="carousel-item active">
+//                 <img src="../public/images/logo.png"  alt="..."/>
+//               </div>
+//               <div className="carousel-item">
+//                 <img src="../public/images/purim-mask-printables-1-alphamom.jpg" style={{width:"fit-content", height:"fit-content"}} alt="..."/>
+//               </div>
+//               <div className="carousel-item">
+//                 <img src="./istockphoto-1385684843-1024x1024.png"  />
+//               </div>
+//             </div>
+//           </div>
+//         </>
+//         );
+// }
+
+// export default Home;
