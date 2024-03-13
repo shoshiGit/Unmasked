2bfb0eee0aab97732239c0cdb72a5b3360bf517a
shoshiHub <shoshiwolpe@gmail.com>
2024-03-13 14:15:36 +0200

    fith commit

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,11 @@
 const Home = () => {
     return (<>
-    <h1>"Unleash the Magic, Embrace the Fun! Unmasked: Where Purim Dreams Come Alive."</h1>
+    <h1>Unleash the Magic<br/> Embrace the Fun!<br/> Unmasked: Where Purim Dreams Come Alive."</h1>
     <p>
   
 
 
-Advertisement:
+
 Step into a world of enchantment with Unmasked, your ultimate destination for the most delightful Purim costumes for kids! At Unmasked, we believe in turning every Purim celebration into a magical experience for your little ones. Our carefully curated collection features an array of vibrant and imaginative costumes that will transport your child to their favorite realms of fantasy. From brave superheroes to graceful princesses, Unmasked offers a kaleidoscope of choices to make this Purim truly special.
 
 Immerse your children in the joy of creativity as they transform into their favorite characters with our high-quality costumes, designed for comfort and durability. At Unmasked, we understand the importance of every detail, ensuring that each costume captures the essence of the character while providing a comfortable and enjoyable wearing experience.
@@ -26,7 +26,7 @@ export default Home;
 //               </div>
 //               <div className="carousel-item">
 //                 <img src="../public/images/purim-mask-printables-1-alphamom.jpg" style={{width:"fit-content", height:"fit-content"}} alt="..."/>
-//               </div>
+//npm start               </div>
 //               <div className="carousel-item">
 //                 <img src="./istockphoto-1385684843-1024x1024.png"  />
 //               </div>
